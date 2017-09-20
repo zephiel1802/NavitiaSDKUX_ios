48f121c998eab177c1b8c7488650f22cfd5ac24e
Chakkra CHAK <chakkra.chak@canaltp.fr>
2017-09-20 19:41:05 +0200

    Reorganizing files

--- NavitiaSDKUX/Classes/Screens/JourneySolutionRoadmapScreen.swift
+++ NavitiaSDKUX/Classes/Screens/JourneySolutionRoadmapScreen.swift
@@ -14,21 +14,26 @@ open class JourneySolutionRoadmapScreen: ComponentView<JourneySolutionRoadmapSta
 
     override open func render() -> NodeType {
         return ComponentNode(ScreenComponent(), in: self).add(children: [
-            ComponentNode(ScreenHeaderComponent(), in: self, props: { (component, hasKey: Bool) in
+            ComponentNode(ScreenHeaderComponent(), in: self, props: { (component: ScreenHeaderComponent, hasKey: Bool) in
                 component.navigationController = self.navigationController
-                component.styles = self.headerStyles
+                component.styles = [
+                    "backgroundColor": config.colors.tertiary,
+                    "paddingTop": 40
+                ]
             }),
-            ComponentNode(ContainerComponent(), in: self, props: { (component, hasKey: Bool) in
-                component.styles = self.summaryStyles
+            ComponentNode(ContainerComponent(), in: self, props: { (component: ContainerComponent, hasKey: Bool) in
+                component.styles = [
+                    "marginTop": -40
+                ]
             }).add(children: [
-                ComponentNode(JourneySolutionComponent(), in: self, props: { (component, hasKey: Bool) in
+                ComponentNode(JourneySolutionComponent(), in: self, props: { (component: JourneySolutionComponent, hasKey: Bool) in
                     component.journey = self.state.journey!
                 })
                 ]),
             ComponentNode(ScrollViewComponent(), in: self).add(children: [
                 ComponentNode(ListViewComponent(), in: self).add(children:
                     self.state.journey!.sections!.map({ (section: Section) -> NodeType in
-                        return ComponentNode(JourneyRoadmapSectionComponent(), in: self, props: { (component, hasKey: Bool) in
+                        return ComponentNode(JourneyRoadmapSectionComponent(), in: self, props: { (component: JourneyRoadmapSectionComponent, hasKey: Bool) in
                             component.section = section
                         })
                     })
@@ -36,13 +41,4 @@ open class JourneySolutionRoadmapScreen: ComponentView<JourneySolutionRoadmapSta
             ])
         ])
     }
-
-    let headerStyles: [String: Any] = [
-        "backgroundColor": config.colors.tertiary,
-        "paddingTop": 40
-    ]
-    
-    let summaryStyles: [String: Any] = [
-        "marginTop": -40
-    ]
 }
