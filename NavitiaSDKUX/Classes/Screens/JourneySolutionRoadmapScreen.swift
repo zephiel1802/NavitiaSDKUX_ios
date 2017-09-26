a1455b979f12a213e439842d31b43f27e78b7124
Chakkra CHAK <chakkra.chak@canaltp.fr>
2017-09-26 17:11:06 +0200

    PR fixes

--- NavitiaSDKUX/Classes/Screens/JourneySolutionRoadmapScreen.swift
+++ NavitiaSDKUX/Classes/Screens/JourneySolutionRoadmapScreen.swift
@@ -16,20 +16,15 @@ open class JourneySolutionRoadmapScreen: ComponentView<JourneySolutionRoadmapSta
         return ComponentNode(ScreenComponent(), in: self).add(children: [
             ComponentNode(ScreenHeaderComponent(), in: self, props: { (component: ScreenHeaderComponent, hasKey: Bool) in
                 component.navigationController = self.navigationController
-                component.styles = [
-                    "backgroundColor": config.colors.tertiary,
-                    "paddingTop": 40
-                ]
+                component.styles = self.screenHeaderStyle
             }),
             ComponentNode(ContainerComponent(), in: self, props: { (component: ContainerComponent, hasKey: Bool) in
-                component.styles = [
-                    "marginTop": -40
-                ]
+                component.styles = self.journeySolutionCard
             }).add(children: [
                 ComponentNode(JourneySolutionComponent(), in: self, props: { (component: JourneySolutionComponent, hasKey: Bool) in
                     component.journey = self.state.journey!
                 })
-                ]),
+            ]),
             ComponentNode(ScrollViewComponent(), in: self).add(children: [
                 ComponentNode(ListViewComponent(), in: self).add(children:
                     self.state.journey!.sections!.map({ (section: Section) -> NodeType in
@@ -41,4 +36,12 @@ open class JourneySolutionRoadmapScreen: ComponentView<JourneySolutionRoadmapSta
             ])
         ])
     }
+
+    let screenHeaderStyle: [String: Any] = [
+        "backgroundColor": config.colors.tertiary,
+        "paddingTop": 40
+    ]
+    let journeySolutionCard: [String: Any] = [
+        "marginTop": -40
+    ]
 }
