12fafa724160a896ad8826084440b64a5f087937
Johan Rouve <johan.rouve@gmail.com>
2017-10-20 16:44:12 +0200

    Add back button on root controller + Fix Carthage project

--- NavitiaSDKUX/Classes/Controllers/ViewController.swift
+++ NavitiaSDKUX/Classes/Controllers/ViewController.swift
@@ -11,24 +11,45 @@ import Render
 
 open class ViewController: UIViewController {
     var bundle: Bundle = Bundle.main
-    
+
     override open var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
-    
+
     override open func viewDidLoad() {
         super.viewDidLoad()
         bundle = Bundle.init(for: self.classForCoder)
         UIFont.registerFontWithFilenameString(filenameString: "SDKIcons.ttf", bundle: bundle)
         view.backgroundColor = config.colors.lighterGray
         title = String(describing: type(of: self)).replacingOccurrences(of: "ViewController", with: "")
+
+        if (navigationController?.viewControllers.first === self && presentingViewController !== nil) {
+            let backButton = UIButton(frame: CGRect(x: 0, y: 0, width: 70, height: 70))
+            let backImage = UIImage(named: "ios_back", in: bundle, compatibleWith: nil)
+            backButton.setImage(backImage, for: .normal)
+            backButton.contentEdgeInsets = UIEdgeInsets(top: 0, left: -32, bottom: 0, right: 0)
+            backButton.titleEdgeInsets = UIEdgeInsets(top: 10, left: 16, bottom: 10, right: 0)
+            backButton.setTitle("Back", for: .normal)
+            backButton.addTarget(self, action: #selector(closeRootController), for: .touchUpInside)
+            let backBarButton = UIBarButtonItem(customView: backButton)
+
+            let textColor = navigationController?.navigationBar.tintColor ?? contrastColor(color: NavitiaSDKUXConfig.getTertiaryColor())
+            backButton.tintColor = textColor
+            backButton.setTitleColor(textColor, for: .normal)
+
+            self.navigationItem.leftBarButtonItem = backBarButton
+        }
+    }
+
+    func closeRootController() {
+        self.dismiss(animated: true, completion: nil)
     }
 }
 
 /// Copy of ComponentController
 /// Just override onLayout method but addComponentToViewControllerHierarchy seems to be redeclared
 public extension ComponentController where Self: UIViewController {
-    
+
     /// Adds the component to the view hierarchy.
     public func addComponentToViewControllerHierarchy() {
         component.onLayoutCallback = { [weak self] duration, component, size in
@@ -39,7 +60,7 @@ public extension ComponentController where Self: UIViewController {
         }
         configureComponentProps()
     }
-    
+
     /// By default the component is centered in the view controller main view.
     /// Overrid this method for a custom layout.
     public func onLayout(duration: TimeInterval, component: AnyComponentView, size: CGSize) {
