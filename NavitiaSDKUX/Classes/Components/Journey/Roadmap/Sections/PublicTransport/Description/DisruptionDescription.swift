15093d0b95c34925fbc66304c674aa71dd56236f
Chakkra CHAK <chakkra.chak@canaltp.fr>
2017-12-01 17:42:04 +0100

    Icon and cause

--- NavitiaSDKUX/Classes/Components/Journey/Roadmap/Sections/PublicTransport/Description/DisruptionDescription.swift
+++ NavitiaSDKUX/Classes/Components/Journey/Roadmap/Sections/PublicTransport/Description/DisruptionDescription.swift
@@ -6,31 +6,31 @@ extension Components.Journey.Roadmap.Sections.PublicTransport.Description {
     class DisruptionDescription: ViewComponent {
         var section: Section?
         var disruptions: [Disruption]?
-        let modes: Modes = Modes()
 
         override func render() -> NodeType {
             return ComponentNode(ViewComponent(), in: self, props: { (component: ViewComponent, hasKey: Bool) in
-                component.styles = self.containerStyles
-            }).add(children: [
-                ComponentNode(DisruptionBadgeComponent(), in: self, props: { (component: DisruptionBadgeComponent, _: Bool) in
-                    component.disruptions = self.disruptions
-                }),
-                ComponentNode(LabelComponent(), in: self, props: { (component: LabelComponent, hasKey: Bool) in
-                    component.styles = self.modeStyles
-                    component.text = self.modes.getPhysicalMode(section: self.section)
-                }),
-                ComponentNode(LineCodeWithDisruptionStatus(), in: self, props: { (component: LineCodeWithDisruptionStatus, hasKey: Bool) in
-                    component.section = self.section
-                    component.disruptions = self.disruptions
+                component.styles = self.styles
+            }).add(children: disruptions!.map { disruption -> NodeType in
+                return ComponentNode(ViewComponent(), in: self, props: { (component: ViewComponent, hasKey: Bool) in
+                    component.styles = self.containerStyles
                 })
-            ])
+                    .add(children: [
+                        ComponentNode(DisruptionBadgeComponent(), in: self, props: { (component: DisruptionBadgeComponent, _: Bool) in
+                            component.disruptions = self.disruptions
+                        }),
+                        ComponentNode(LabelComponent(), in: self, props: { (component: LabelComponent, hasKey: Bool) in
+                            component.styles = self.causeStyles
+                            component.text = disruption.cause!
+                        })
+                    ])
+            })
         }
 
         let containerStyles: [String: Any] = [
             "alignItems": YGAlign.center,
             "flexDirection": YGFlexDirection.row,
         ]
-        let modeStyles: [String: Any] = [
+        let causeStyles: [String: Any] = [
             "fontSize": 15,
             "marginRight": 5,
         ]
