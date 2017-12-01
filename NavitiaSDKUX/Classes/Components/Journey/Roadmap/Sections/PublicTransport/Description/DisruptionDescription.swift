cf554f2d95ddee2a957e4abb090131fa0a89b270
Chakkra CHAK <chakkra.chak@canaltp.fr>
2017-12-01 18:33:44 +0100

    Added container in LabelComponent

--- NavitiaSDKUX/Classes/Components/Journey/Roadmap/Sections/PublicTransport/Description/DisruptionDescription.swift
+++ NavitiaSDKUX/Classes/Components/Journey/Roadmap/Sections/PublicTransport/Description/DisruptionDescription.swift
@@ -9,10 +9,10 @@ extension Components.Journey.Roadmap.Sections.PublicTransport.Description {
 
         override func render() -> NodeType {
             return ComponentNode(ViewComponent(), in: self, props: { (component: ViewComponent, hasKey: Bool) in
-                component.styles = self.styles
+                component.styles = self.containerStyles
             }).add(children: disruptions!.map { disruption -> NodeType in
                 return ComponentNode(ViewComponent(), in: self, props: { (component: ViewComponent, hasKey: Bool) in
-                    component.styles = self.containerStyles
+                    component.styles = self.disruptionContainerStyles
                 })
                     .add(children: [
                         ComponentNode(DisruptionBadgeComponent(), in: self, props: { (component: DisruptionBadgeComponent, _: Bool) in
@@ -28,12 +28,15 @@ extension Components.Journey.Roadmap.Sections.PublicTransport.Description {
         }
 
         let containerStyles: [String: Any] = [
+            "paddingTop": 24,
+        ]
+        let disruptionContainerStyles: [String: Any] = [
             "alignItems": YGAlign.center,
             "flexDirection": YGFlexDirection.row,
         ]
         let causeStyles: [String: Any] = [
             "fontSize": 15,
-            "marginRight": 5,
+            "paddingLeft": 5,
             "fontWeight": "bold"
         ]
     }
