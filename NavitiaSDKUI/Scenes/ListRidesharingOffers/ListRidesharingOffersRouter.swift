aa89008b411d191a002ba6a96687aa1b91dbfb89
Flavien SICARD <flavien.sicard@kisio.org>
2018-09-14 16:55:23 +0200

    🐛Fix .pbxproj

--- NavitiaSDKUI/Scenes/ListRidesharingOffers/ListRidesharingOffersRouter.swift
+++ NavitiaSDKUI/Scenes/ListRidesharingOffers/ListRidesharingOffersRouter.swift
@@ -9,6 +9,7 @@ import Foundation
 
 protocol ListRidesharingOffersRoutingLogic {
     
+    func routeToShowJourneyRoadmap()
 }
 
 protocol ListRidesharingOffersDataPassing {
@@ -20,4 +21,33 @@ class ListRidesharingOffersRouter: NSObject, ListRidesharingOffersRoutingLogic, 
     
     weak var viewController: ListRidesharingOffersViewController?
     var dataStore: ListRidesharingOffersDataStore?
+    
+    // MARK: Routing
+    
+    func routeToShowJourneyRoadmap() {
+        guard let viewController = viewController, let dataStore = dataStore else {
+            return
+        }
+        
+        let destinationVC = viewController.storyboard?.instantiateViewController(withIdentifier: ShowJourneyRoadmapViewController.identifier) as! ShowJourneyRoadmapViewController
+        var destinationDS = destinationVC.router!.dataStore!
+        
+        passDataToShowJourneyRoadmap(source: dataStore, destination: &destinationDS)
+        navigateToShowJourneyRoadmap(source: viewController, destination: destinationVC)
+    }
+    
+    // MARK: Navigation
+
+    func navigateToShowJourneyRoadmap(source: ListRidesharingOffersViewController, destination: ShowJourneyRoadmapViewController) {
+        source.navigationController?.pushViewController(destination, animated: true)
+    }
+    
+    // MARK: Passing Data
+    
+    func passDataToShowJourneyRoadmap(source: ListRidesharingOffersDataStore, destination: inout ShowJourneyRoadmapDataStore) {
+        destination.journey = source.journey
+        destination.disruptions = source.disruptions
+        destination.notes = source.notes
+        destination.context = source.context
+    }
 }
