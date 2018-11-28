baeab4b87e4a89eeda9df797cf0c03bb2db563f8
Flavien SICARD <flavien.sicard@kisio.org>
2018-11-28 15:05:40 +0100

    ♻️ Clear code

--- NavitiaSDKUI/Framedation/Extension/UIImageView+Extension.swift
+++ NavitiaSDKUI/Framedation/Extension/UIImageView+Extension.swift
@@ -25,5 +25,4 @@ extension UIImageView {
             }).resume()
         }
     }
-    
 }
--- NavitiaSDKUI/Framedation/Extension/UIViewController+Extension.swift
+++ NavitiaSDKUI/Framedation/Extension/UIViewController+Extension.swift
@@ -12,26 +12,27 @@ extension UIViewController {
     func addBackButton(targetSelector: Selector) {
         if isRootViewController() {
             let backButton = UIButton(frame: CGRect(x: 0, y: 0, width: 70, height: 70))
+            
             backButton.setImage(UIImage(named: "back-icon", in: NavitiaSDKUI.shared.bundle, compatibleWith: nil)?.withRenderingMode(.alwaysTemplate), for: .normal)
             backButton.contentEdgeInsets = UIEdgeInsets(top: 0, left: -32, bottom: 0, right: 0)
             backButton.titleEdgeInsets = UIEdgeInsets(top: 10, left: 16, bottom: 10, right: 0)
             backButton.setTitle("back".localized(), for: .normal)
             backButton.addTarget(self, action: targetSelector, for: .touchUpInside)
             backButton.tintColor = Configuration.Color.main.contrastColor()
-            self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: backButton)
             
+            navigationItem.leftBarButtonItem = UIBarButtonItem(customView: backButton)
+            
+            navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: Configuration.Color.main.contrastColor()]
             UINavigationBar.appearance().backgroundColor = Configuration.Color.main
             UIBarButtonItem.appearance().tintColor = Configuration.Color.main.contrastColor()
-            self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: Configuration.Color.main.contrastColor()]
         }
     }
     
     func isRootViewController() -> Bool {
-        if self.navigationController != nil && self.navigationController!.viewControllers.count > 1 {
+        if let navigationController = navigationController, navigationController.viewControllers.count > 1 {
             return false
         }
         
         return true
     }
-    
 }
