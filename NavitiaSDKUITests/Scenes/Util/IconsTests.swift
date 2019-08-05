982c21ddb613e68eed4875d150325ef82b141810
Adeline H <adelinehirsch@gmail.com>
2019-08-05 17:28:56 +0200

    Utils - icon tests

--- NavitiaSDKUITests/Scenes/Util/IconsTests.swift
+++ NavitiaSDKUITests/Scenes/Util/IconsTests.swift
@@ -6,8 +6,12 @@
 //
 
 import XCTest
+@testable import NavitiaSDKUI
 
 class IconsTests: XCTestCase {
+    
+    let unknownIconString = "\u{ffff}"
+    let walkingIconString = "\u{ea25}"
     
     override func setUp() {
         super.setUp()
@@ -17,5 +21,39 @@ class IconsTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+    }
+    
+    // Mode Icon
+    func testModeIconNameEmpty() {
+        XCTAssertEqual(Icon("").iconFontCode, unknownIconString)
+    }
+    
+    func testModeIconNameUnknown() {
+        XCTAssertEqual(Icon("toto").iconFontCode, unknownIconString)
+    }
+    
+    func testModeIconNameWalking() {
+        XCTAssertEqual(Icon("walking").iconFontCode, walkingIconString)
+    }
+    
+    // DisruptionIcon
+    func testDisruptionLevelIconUnknown() {
+        XCTAssertEqual(Disruption().levelImage(name:"toto"), nil)
+    }
+    
+    func testDisruptionColorUnknown() {
+        XCTAssertEqual(Disruption.levelColor(of: .none), "000000")
+    }
+    
+    func testDisruptionColorInformation() {
+        XCTAssertEqual(Disruption.levelColor(of: .information), "43B77A")
+    }
+    
+    func testDisruptionColorNonblocking() {
+        XCTAssertEqual(Disruption.levelColor(of: .nonblocking), "EF662F")
+    }
+    
+    func testDisruptionColorBlocking() {
+        XCTAssertEqual(Disruption.levelColor(of: .blocking), "FF0000")
     }
 }
