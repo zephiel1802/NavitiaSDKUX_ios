31956824446ed609b2fd394f6f8f796391ee76b0
Mickael PETITFILS <282292+MikeFr@users.noreply.github.com>
2019-09-27 16:01:39 +0200

    Fix UI bug frieze overlapping walking information

--- NavitiaSDKUI/Scenes/ListJourneys/ListJourneysViewController.swift
+++ NavitiaSDKUI/Scenes/ListJourneys/ListJourneysViewController.swift
@@ -467,11 +467,11 @@ extension ListJourneysViewController: ListJourneysCollectionViewLayoutDelegate {
             
             var height: CGFloat = 60
             if let _ = viewModel.displayedJourneys[safe: indexPath.row]?.walkingInformation {
-                height += 30 // TODO : Fix height of cell
+                height += 30
             }
             
             if let sections = viewModel.displayedJourneys[safe: indexPath.row]?.friezeSections {
-                let friezeView = FriezeView(frame: CGRect(x: 0, y: 0, width: width - 20, height: 27))
+                let friezeView = FriezeView(frame: CGRect(x: 0, y: 0, width: width - 66, height: 27))
                 friezeView.addSection(friezeSections: sections)
                 
                 return height + friezeView.frame.size.height
